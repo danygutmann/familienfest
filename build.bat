946f7371d9257ef00b8c9fd688caffd2b318a5ef
danygutmann <DGutmann@ad.harman.com>
2021-04-27 13:17:04 +0200

    commit

--- build.bat
+++ build.bat
@@ -4,5 +4,3 @@ dotnet publish -c Release -o release
 xcopy "C:\GIT\familienfest\repro\familienfest\release\wwwroot" "C:\GIT\familienfest\repro\familienfest\docs" /s /e /h /Y
 
 BlazorReplaceRoot.exe "C:\GIT\familienfest\repro\familienfest\docs\index.html" "familienfest"
-
-pause
